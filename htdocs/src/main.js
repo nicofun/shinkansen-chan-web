a8d3647c0547313ba6de3d21858c58df6c76796a
Reo Sato <isaac@nicofun.club>
2016-09-24 17:27:45 +0900

    Add Live2D

--- htdocs/src/main.js
+++ htdocs/src/main.js
@@ -1,11 +1,23 @@
+window.onload = function() {
+	sampleApp1();
+};
+
 var socketio = io.connect(document.domain + ":8000")
 
-socketio.on("chat", function(data) { console.log(data.value) });
+socketio.on("chat", function(data) { addTweet(">> " + data.value); });
 
 function tweet() {
 	var tweetMsg = document.getElementById('tweetMsg');
 	if(tweetMsg.value !== "") {
+		addTweet("<< " + tweetMsg.value);
 		socketio.emit("chat", {value: tweetMsg.value});
 		tweetMsg.value = "";
 	}
+}
+
+function addTweet(chat) {
+	var tweetArea = document.getElementById('tweetArea');
+	var tweet = document.createElement('p');
+	tweet.innerHTML = chat;
+	tweetArea.appendChild(tweet);
 }
